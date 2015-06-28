03862638d3efb5836a7c56add886509b44ce8ed3
Travis Briggs <audiodude@gmail.com>
2015-06-28 19:35:52 +0000

    Producer creates base64 encoded MIDI file and inserts into Mongo.

--- neural-noise-producer/produce.py
+++ neural-noise-producer/produce.py
@@ -1,19 +1,26 @@
+import base64
+import os
+import re
 import subprocess
+import tempfile
 import time
 
 import pymongo
 
-CHAR_RNN_DIR='/home/music/char-rnn'
-DATA_LENGTH=1024
+CHAR_RNN_DIR = '/home/music/char-rnn'
+CP_FILE = 'lm_lstm_epoch18.85_0.3871.t7'
+DATA_LENGTH = 8192
 
 MONGO_URL = 'mongodb://localhost:27017/neural-noise'
 client = pymongo.MongoClient(MONGO_URL)
 db = client.get_default_database()
 
+RE_REPEAT = re.compile('\|:(.*?):\|')
+
 def get_generated_data(temperature):
-  cmdline = ('th sample.lua cv/lm_lstm_epoch0.99_0.5225.t7 -primetext "X:1" '
-             '-length %(length)s -temperature %(temp)s -gpuid -1 -verbose 0 '
-             '-seed %(seed)s' % {
+  cmdline = ('th sample.lua cv/%(file)s -primetext "X:1" -length %(length)s '
+             '-temperature %(temp)s -gpuid -1 -verbose 0 -seed %(seed)s' % {
+               'file': CP_FILE,
                'length': DATA_LENGTH,
                'temp': temperature,
                'seed': int(time.time())
@@ -25,23 +32,54 @@ def get_generated_data(temperature):
   data, _ = pipe.communicate()
   return data
 
-def main():
-  data = get_generated_data('0.8')
-  print data
-  print '--------------------'
+section_count = 0
+def get_songs(temperature):
+  global section_count
+  data = get_generated_data(temperature)
   songs = data.split('X:1\n')
-  if songs[-1].find(':|') == -1:
-    songs = songs[:-1]
-
   for song in songs:
     if not song:
       continue
-    song = song.replace('|:', '').replace(':|', '')
-    print song
-    print '======'
-    # db.songs.insert({
-    #   'abc': song
-    # })
+
+    section_count = 0
+    def process_section(md):
+      global section_count
+      if md.group(1):
+        section_count += 1
+      return md.group(1)
+    song = RE_REPEAT.sub(process_section, song)
+    if section_count < 2:
+      continue
+    song = 'X:1\n' + song
+    yield song
+
+def get_midi_path(song):
+  with tempfile.NamedTemporaryFile() as f:
+    f.write(song)
+    f.flush()
+    dest_path = os.path.join('/tmp', os.path.basename(f.name) + '.mid')
+    args = ['/home/music/abcmidi/abc2midi', f.name, '-o', dest_path, '-silent']
+    print ' '.join(args)
+    subprocess.call(args)
+    return dest_path
+
+def insert_songs(temperature):
+  count = 0
+  for song in get_songs(temperature):
+    count += 1
+    midi_path = get_midi_path(song)
+
+    with open(midi_path) as f:
+      midi_bin = f.read()
+    midi = base64.b64encode(midi_bin)
+
+    db.songs.insert({
+      'abc': song,
+      'midi': midi,
+    })
+  return count
 
 if __name__ == '__main__':
-  main()
+  temperature = '0.85'
+  num = insert_songs(temperature)
+  print '%s songs inserted, temperature=%s' % (num, temperature)
