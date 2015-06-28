0044d61ce1f453fee5416246ba25e9a57a80ff90
Travis Briggs <audiodude@gmail.com>
2015-06-28 21:04:15 +0000

    Working retrieval of songs and MIDI playback.

--- neural-noise-web/app.py
+++ neural-noise-web/app.py
@@ -1,6 +1,14 @@
+import decimal
 import os
 
+import bson
 import flask
+import pymongo
+
+
+MONGO_URL = 'mongodb://localhost:27017/neural-noise'
+client = pymongo.MongoClient(MONGO_URL)
+db = client.get_default_database()
 
 app = flask.Flask(__name__)
 
@@ -12,9 +20,21 @@ def index():
   return flask.render_template('index.html')
 
 @app.route('/query')
+def query():
+  temperature = flask.request.args['temperature']
+  song = db.songs.find_one({
+    'temperature': { '$eq': temperature }
+  })
+  return flask.redirect(flask.url_for('render', x=str(song['_id'])))
+
+@app.route('/render')
 def render():
-  temp = flask.request.args['temperature']
-  return'Query temperature = %s' % temp
+  id_ = flask.request.args['x']
+  song = db.songs.find_one({
+    '_id': { '$eq': bson.objectid.ObjectId(id_) }
+  })
+  print song['midi']
+  return flask.render_template('render.html', song=song)
 
 if __name__ == '__main__':
   app.run(debug=is_debug(), host='0.0.0.0')
