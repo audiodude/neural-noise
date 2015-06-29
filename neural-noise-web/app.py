7151c4383517137beec98a3c85e21efb59f3b898
Travis Briggs <audiodude@gmail.com>
2015-06-29 06:03:09 +0000

    Choose songs randomly.Xg

--- neural-noise-web/app.py
+++ neural-noise-web/app.py
@@ -1,5 +1,6 @@
 import decimal
 import os
+import random
 
 import bson
 import flask
@@ -15,6 +16,26 @@ app = flask.Flask(__name__)
 def is_debug():
   return os.environ.get('FLASK_ENV') != 'production'
 
+def random_song(temperature):
+  # Taken from http://stackoverflow.com/a/21867984/41060
+  query = {
+    'temperature': { '$eq': temperature },
+    'random': { '$lte': random.random() },
+  }
+  cursor = db.songs.find(query).sort('random', pymongo.DESCENDING)
+  try:
+    doc = next(cursor)
+  except StopIteration:
+    cursor = db.songs.find().sort('random', pymongo.DESCENDING);
+    try:
+      doc = next(cursor)
+    except StopIteration:
+      return None
+
+  doc['random'] = random.random();
+  db.songs.update({ '_id': doc['_id'] }, doc);
+  return doc
+
 @app.route('/')
 def index():
   return flask.render_template('index.html')
@@ -22,9 +43,9 @@ def index():
 @app.route('/query')
 def query():
   temperature = flask.request.args['temperature']
-  song = db.songs.find_one({
-    'temperature': { '$eq': temperature }
-  })
+  song = random_song(temperature)
+  if not song:
+    return 'Error: no songs with temperature=%s found' % temperature
   return flask.redirect(flask.url_for('render', x=str(song['_id'])))
 
 @app.route('/render')
@@ -33,7 +54,7 @@ def render():
   song = db.songs.find_one({
     '_id': { '$eq': bson.objectid.ObjectId(id_) }
   })
-  print song['midi']
+
   return flask.render_template('render.html', song=song)
 
 if __name__ == '__main__':
