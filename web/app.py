f2b40e67f79c437672ede7cc250aec68bfa4a202
Travis Briggs <audiodude@gmail.com>
2015-07-01 04:55:25 +0000

    Expose checkpoints in the web app.

--- web/app.py
+++ web/app.py
@@ -16,9 +16,10 @@ app = flask.Flask(__name__)
 def is_debug():
   return os.environ.get('FLASK_ENV') != 'production'
 
-def random_song(temperature):
+def random_song(checkpoint, temperature):
   # Taken from http://stackoverflow.com/a/21867984/41060
   query = {
+    'checkpoint': { '$eq': checkpoint },
     'temperature': { '$eq': temperature },
     'random': { '$lte': random.random() },
   }
@@ -26,7 +27,8 @@ def random_song(temperature):
   try:
     doc = next(cursor)
   except StopIteration:
-    cursor = db.songs.find().sort('random', pymongo.DESCENDING);
+    del query['random']
+    cursor = db.songs.find(query).sort('random', pymongo.DESCENDING);
     try:
       doc = next(cursor)
     except StopIteration:
@@ -38,18 +40,22 @@ def random_song(temperature):
 
 @app.route('/')
 def index():
-  return flask.render_template('index.html')
+  checkpoints = [cp['name'] for cp in db.checkpoints.find({})]
+  return flask.render_template('index.html', checkpoints=checkpoints)
 
 @app.route('/query')
 def query():
   temperature = flask.request.args['temperature']
-  song = random_song(temperature)
+  checkpoint = flask.request.args['checkpoint']
+  print 'Checkpoint %s temperature %s' % (checkpoint, temperature)
+  song = random_song(checkpoint, temperature)
   if not song:
-    return 'Error: no songs with temperature=%s found' % temperature
-  return flask.redirect(flask.url_for('render', id_=str(song['_id'])))
+    return 'Error: no songs at checkpoint=%swith temperature=%s found' % (checkpoint, temperature)
+  return flask.redirect(flask.url_for(
+    'render', checkpoint=checkpoint, id_=str(song['_id'])))
 
-@app.route('/render/<id_>')
-def render(id_):
+@app.route('/render/<checkpoint>/<id_>')
+def render(checkpoint, id_):
   song = db.songs.find_one({
     '_id': { '$eq': bson.objectid.ObjectId(id_) }
   })
